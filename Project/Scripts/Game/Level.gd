c5f26ae87371892aa81b2c2d9c8d38d26b727914
Tan Kok Liang <tobia88@gmail.com>
2024-08-18 14:13:20 +0800

    Fix few bugs and inheritance

--- Project/Scripts/Game/Level.gd
+++ Project/Scripts/Game/Level.gd
@@ -58,7 +58,6 @@ func on_character_enter_building(character: Character, building: Building) -> vo
 	character.scale_lvl += 1
 	character.launch_by_target_height(5.0)
 
-
 func on_character_collide_structure(character: Character, structure: TargetStructure) -> void:
 	if character.scale_lvl < structure.required_scale_lvl:
 		character.dead()
--- Project/Scripts/Global/Manager/GameManager.gd
+++ Project/Scripts/Global/Manager/GameManager.gd
@@ -1,14 +1,23 @@
 extends Node
 
+var level_template_arr: Array[PackedScene] = [
+	preload("res://Levels/Level_0.tscn"),
+	preload("res://Levels/Level_1.tscn"),
+]
+
 var _active_level: Level
 var active_level: Level:
 	get:
-		if not _active_level:
-			_active_level = get_tree().get_first_node_in_group("level")
+		_active_level = get_tree().current_scene as Level
 		return _active_level
 	set(value):
 		_active_level = active_level
 
+
 func _ready() -> void:
-	if active_level:
-		active_level.level_state = Level.LevelState.GAME_START
+	active_level.level_state = Level.LevelState.GAME_START
+
+func load_level(idx: int) -> Level:
+	var new_lvl = level_template_arr[idx].instantiate() as Level
+	assert(new_lvl)
+	return new_lvl
