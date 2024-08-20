3cd4f788d8a74e2c6246a9b380596e6d0bec53ff
Tan Kok Liang <tobia88@gmail.com>
2024-08-20 17:06:23 +0800

    Stage by stage submit

--- Project/Scripts/Gimmick/Gimmick_Hole.gd
+++ Project/Scripts/Gimmick/Gimmick_Hole.gd
@@ -6,24 +6,23 @@ class_name Gimmick_Hole
 
 @onready var animation_tree: AnimationTree = $AnimationTree
 @onready var emission_mesh: MeshInstance3D = $Mesh_Gimmick_Windhole/Armature/Skeleton3D/Cylinder
+@onready var animation_player: AnimationPlayer = $VFX_Windhole_Blow/AnimationPlayer
 
-var default_trigger_col: Color
-var is_active: float = 0.0
-
+var is_active := 0.0
+var last_is_operated := false
 
 func _ready():
-	#default_trigger_col = $MeshInstance3D/MeshInstance3D2.get_active_material(0)["shader_parameter/MainColor"]
-	pass
+	last_is_operated = is_operated
 	
 
 func _process(delta: float) -> void:
-	#var material: Material = $MeshInstance3D/MeshInstance3D2.get_active_material(0)
-	#material["shader_parameter/MainColor"] = Color.BLACK if is_operated else default_trigger_col
 	$Area3D.monitoring = not is_operated
 	is_active = move_toward(is_active, !is_operated, delta * 10.0)
 	_update_animations()
 	_update_materials()
+	_update_particles()
 
+	last_is_operated = is_operated
 
 func _update_animations() -> void:
 	animation_tree["parameters/StateMachine/conditions/is_blowing"] = is_operated
@@ -32,6 +31,12 @@ func _update_animations() -> void:
 func _update_materials() -> void:
 	var mat : ShaderMaterial = emission_mesh.get_surface_override_material(1)
 	mat.set_shader_parameter("is_active", is_active)
+
+
+func _update_particles() -> void:
+	if last_is_operated != is_operated and is_operated:
+		animation_player.active = true
+		animation_player.play("Activate")
 
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
