d92249596b1e504e84b35514dd3c81e3aa9f3cc8
Tan Kok Liang <tobia88@gmail.com>
2024-08-20 23:40:44 +0800

    Add particle

--- Project/Scripts/TargetStructure/TargetStructure.gd
+++ Project/Scripts/TargetStructure/TargetStructure.gd
@@ -12,4 +12,8 @@ func _process(delta: float) -> void:
 
 func dead() -> void:
 	AudioManager.play_sfx($AudioStreamPlayer_Explosion.stream)
+	$Node3D/GPUParticles3D.emitting = true
+	$Node3D/GPUParticles3D.reparent(get_tree().current_scene)
+	$Node3D/GPUParticles3D2.emitting = true
+	$Node3D/GPUParticles3D2.reparent(get_tree().current_scene)
 	queue_free()
